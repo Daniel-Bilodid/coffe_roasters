7af5bbccf822aef94a30735390a4a12870e4d54b
Даніель Білодід <danielbeloded@gmail.com>
2025-01-14 13:48:27 +0200

    working with last page

---
+++ src/components/planPage/planPick/AppPlanPick.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="flex mt-[168px]">
+    <div>
+      <ul class="w-[255px] h-[356px] ml-[85px]">
+        <li
+          class="text-left font-fraunces text-2xl font-black text-customBlack cursor-pointer"
+        >
+          <span
+            class="font-fraunces text-2xl font-black text-customGray mr-[28.5px]"
+            >01</span
+          >
+          Preferences
+        </li>
+        <div class="w-full h-[1px] bg-customGray mt-[24px] mb-[24px]"></div>
+        <li
+          class="text-left font-fraunces text-2xl font-black text-customGray cursor-pointer"
+        >
+          <span
+            class="font-fraunces text-2xl font-black text-customGray mr-[28.5px]"
+            >02</span
+          >
+          Bean Type
+        </li>
+        <div class="w-full h-[1px] bg-customGray mt-[24px] mb-[24px]"></div>
+        <li
+          class="text-left font-fraunces text-2xl font-black text-customGray cursor-pointer"
+        >
+          <span
+            class="font-fraunces text-2xl font-black text-customGray mr-[28.5px]"
+            >03</span
+          >
+          Quantity
+        </li>
+        <div class="w-full h-[1px] bg-customGray mt-[24px] mb-[24px]"></div>
+        <li
+          class="text-left font-fraunces text-2xl font-black text-customGray cursor-pointer"
+        >
+          <span
+            class="font-fraunces text-2xl font-black text-customGray mr-[28.5px]"
+            >04</span
+          >
+          Grind Option
+        </li>
+        <div class="w-full h-[1px] bg-customGray mt-[24px] mb-[24px]"></div>
+        <li
+          class="text-left font-fraunces text-2xl font-black text-customGray cursor-pointer"
+        >
+          <span
+            class="font-fraunces text-2xl font-black text-customGray mr-[28.5px]"
+            >05</span
+          >
+          Deliveriers
+        </li>
+      </ul>
+    </div>
+    <div>How do you drink your coffe?</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppPlanPick",
+};
+</script>
--- src/pages/AppPlan.vue
+++ src/pages/AppPlan.vue
@@ -1,18 +1,20 @@
 <template>
   <AppPlanHero />
   <AppPlanWork />
+  <AppPlanPick />
 </template>
 
 <script>
 import AppPlanHero from "@/components/planPage/planHero/AppPlanHero.vue";
-
 import AppPlanWork from "@/components/planPage/planWork/AppPlanWork.vue";
+import AppPlanPick from "@/components/planPage/planPick/AppPlanPick.vue";
 
 export default {
   name: "AppPlan",
   components: {
     AppPlanHero,
     AppPlanWork,
+    AppPlanPick,
   },
 };
 </script>
