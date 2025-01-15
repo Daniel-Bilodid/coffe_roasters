0dcabb22285ebca904a6ea9cd5ab0df066082d39
Даніель Білодід <danielbeloded@gmail.com>
2025-01-15 20:04:58 +0200

    almost finished with app logic

--- src/components/planPage/planPick/AppPlanPick.vue
+++ src/components/planPage/planPick/AppPlanPick.vue
@@ -80,23 +80,52 @@
             :class="[
               'w-[228px] h-[250px] rounded-[8px] cursor-pointer mb-[88px]',
               selectedCards[sectionIndex] === card.title
-                ? 'bg-customGreen'
+                ? 'bg-customGreen '
                 : 'bg-customCream',
             ]"
             @click="selectCard(sectionIndex, card.title)"
           >
             <div
-              class="font-fraunces font-black text-2xl mt-[32px] ml-[28px] text-left"
+              :class="[
+                'font-fraunces font-black text-2xl mt-[32px] ml-[28px] text-left',
+                selectedCards[sectionIndex] === card.title
+                  ? 'text-white '
+                  : 'text-black',
+              ]"
             >
               {{ card.title }}
             </div>
             <div
-              class="font-barlow text-base text-customBlack text-left mt-[24px] ml-[28px] w-[172px] h-[78px]"
+              :class="[
+                'font-barlow text-base text-customBlack text-left mt-[24px] ml-[28px] w-[172px] h-[78px]',
+                selectedCards[sectionIndex] === card.title
+                  ? 'text-white '
+                  : 'text-black',
+              ]"
             >
               {{ card.description }}
             </div>
           </div>
         </div>
+      </div>
+      <div class="w-[730px] h-[208px] bg-customDarkBlue rounded-[10px]">
+        <div
+          class="text-white opacity-[0.5] font-barlow text-base uppercase text-left ml-[64px] mt-[31px]"
+        >
+          Order Summary
+        </div>
+        <div
+          class="ml-[64px] mr-[64px] text-white font-fraunces font-black font-2xl"
+        >
+          “I drink my coffee as
+          {{ selectedCards[0] || "..." }}, with a
+          {{ selectedCards[1] || "..." }} type of bean.
+          {{ selectedCards[2] || "..." }} ground ala
+          {{ selectedCards[3] || "..." }}, sent to me
+          {{ selectedCards[4] || "..." }}”
+        </div>
+
+        {{ console.log("select", selectedCards) }}
       </div>
     </div>
   </div>
@@ -105,6 +134,7 @@
 <script>
 import { ref } from "vue";
 import { coffeeOptions } from "@/data/coffeOptions";
+
 import iconArrow from "@/assets/plan/desktop/icon-arrow.svg";
 
 export default {
@@ -112,7 +142,7 @@ export default {
   setup() {
     const coffeeOptionsData = coffeeOptions;
     const iconArrowImg = iconArrow;
-    const selectedCards = ref(Array(coffeeOptions.length).fill(null));
+    const selectedCards = ref(Array(coffeeOptions.length).fill("..."));
 
     const selectCard = (sectionIndex, cardTitle) => {
       selectedCards.value[sectionIndex] = cardTitle;
